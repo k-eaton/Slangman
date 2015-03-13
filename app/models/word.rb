a42a4a4a9400e0c03146e9af5f683c201d611ded
Katrina Eaton <katrina.eaton@gmail.com>
2015-03-12 17:30:25 -0700

    add word validations

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,1 +1,12 @@
+class Word < ActiveRecord::Base
+
+  belongs_to :list
+
+  validates :word, :definition, :example, { :presence => true }
+
+  validates :word, format: { with: /.{5,}/ }
+  validates :word, format: { without: /[A-Z]{2,}/ }
+  validates :word, format: { without: /\D*\d\D*/ }
+
+end
 
