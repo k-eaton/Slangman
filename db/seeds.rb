5f6f666ab34f50d68152899975263d882f23af54
sollabeck <kevin.ceballos@gmail.com>
2015-03-15 21:03:43 -0700

    Scrape UD everytime game is ran

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,9 @@ require_relative '../app/models/list'
 require_relative '../app/models/word'
 require_relative '../app/controllers/controller'
 
+  Controller.grab_info.each do |each_word|
+    List.first.words << Word.create(word: each_word[:word], definition: each_word[:definition], example: each_word[:example])
+  end
 
-list = List.create
-Controller.grab_info
-Controller.grab_info.each do |each_word|
-  list.words.create(word: each_word[:word], definition: each_word[:definition], example: each_word[:example])
-end
+
  #Array of hashes
--- slangman.rb
+++ slangman.rb
@@ -13,7 +13,11 @@ controller = Controller.new(
   )
 
 
+ Controller.grab_info.each do |each_word|
+    List.first.words << Word.create(word: each_word[:word], definition: each_word[:definition], example: each_word[:example])
+  end
+
 sampled_word = Word.all.sample
-word = Word.new(sampled_word)
-test_game = Hangman.new(word)
+sampled_word.update(word: sampled_word.word.downcase)
+test_game = Hangman.new(sampled_word)
 test_game.play_game
